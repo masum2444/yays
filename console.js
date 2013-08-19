3c75458bf935d196593e9a02e2a5363848b9c566
eugenox <eugenox@gmail.com>
2013-08-19 22:50:10 +0200

    Minor simplification.

--- console.js
+++ console.js
@@ -5,16 +5,16 @@
 var Console = {
 #if RELEASE
 	debug: function() {
-		unsafeWindow.console.debug.apply(unsafeWindow.console, Array.prototype.concat.apply(['[' + Meta.ns + ']'], arguments));
+		unsafeWindow.console.debug(Array.prototype.concat.apply(['[' + Meta.ns + ']'], arguments).join(' '));
 	}
 #else
 	display: document.createElement('pre'),
 
 	debug: function() {
-		var pieces = Array.prototype.concat.apply([], arguments);
+		var message = Array.prototype.join.call(arguments, ' ');
 
-		this.display.appendChild(document.createTextNode(pieces.join(' ') + '\n'));
-		unsafeWindow.console.debug.apply(unsafeWindow.console, ['[' + Meta.ns + ']'].concat(pieces));
+		this.display.appendChild(document.createTextNode(message + '\n'));
+		unsafeWindow.console.debug('[' + Meta.ns + '] ' + message);
 	}
 #endif
 };
