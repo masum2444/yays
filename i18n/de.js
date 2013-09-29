fbbf81c6e9e8f4e5fb5511192a35585b4670ab09
eugenox <eugenox@gmail.com>
2013-09-29 14:20:53 +0200

    Update translations.

--- i18n/de.js
+++ i18n/de.js
@@ -1,3 +1,3 @@
 			// German - xemino, ich01
 			case 'de':
-				return [null, null, null, null, null, null, null, "Qualit\u00e4t", "AUTO", "Original", "Standard Video Qualit\u00e4t setzen", "Gr\u00f6\u00dfe", "Auto", "Breit", "Passend", "Standard Player Gr\u00f6\u00dfe setzen", "Einstellungen", "Hilfe"];
+				return ["Wiedergabe", "Start", "Pause", "Stop", "Auto-Pause", "Auto-Stop", "Standardm\u00e4\u00dfigen Wiedergabezustand setzen", "Qualit\u00e4t", "Auto", "Original", "Standard Video Qualit\u00e4t setzen", "Gr\u00f6\u00dfe", "Auto", "Breit", "Passend", "Standard Player Gr\u00f6\u00dfe setzen", "Einstellungen", "Hilfe"];
