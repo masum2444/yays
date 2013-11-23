a7c02a1c08a97654ae8c10467f41b6145d8e2995
eugenox <eugenox@gmail.com>
2013-11-23 16:38:29 +0100

    Drop cookie support.

--- config.js
+++ config.js
@@ -12,37 +12,18 @@ var Config = (function(namespace) {
 		};
 	}
 
-	var prefix = namespace + '.';
-
 	// HTML5
-	if (typeof unsafeWindow.localStorage != 'undefined') {
-		return {
-			get: function(key) {
-				return unsafeWindow.localStorage.getItem(prefix + key);
-			},
-
-			set: function(key, value) {
-				unsafeWindow.localStorage.setItem(prefix + key, value);
-			},
-
-			del: function(key) {
-				unsafeWindow.localStorage.removeItem(prefix + key);
-			}
-		};
-	}
-
-	// Cookie
 	return {
 		get: function(key) {
-			return (document.cookie.match(new RegExp('(?:^|; *)' + prefix + key + '=(\\w+)(?:$|;)')) || [, null])[1];
+			return unsafeWindow.localStorage.getItem(namespace + '.' + key);
 		},
 
 		set: function(key, value) {
-			document.cookie = prefix.concat(key, '=', value, '; path=/; expires=', new Date(new Date().valueOf() + 365 * 24 * 36e5).toUTCString());
+			unsafeWindow.localStorage.setItem(namespace + '.' + key, value);
 		},
 
 		del: function(key) {
-			document.cookie = prefix.concat(key, '=deleted; path=/; max-age=0');
+			unsafeWindow.localStorage.removeItem(namespace + '.' + key);
 		}
 	};
 })(Meta.ns);
